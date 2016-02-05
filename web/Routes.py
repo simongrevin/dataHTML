5010d10557dbc63e2cffa319409226b6a8f04a1c
klezalaise <arthur.grevin@gmai.com>
2016-02-05 13:56:15 +0100

    Nouvelle branch Arthur

--- web/Routes.py
+++ web/Routes.py
@@ -2,40 +2,38 @@ from flask import Blueprint,jsonify
 
 import imp
 
-beans = imp.load_package("beans","../beans")
+#beans = imp.load_package("beans","../beans")
 services = imp.load_package("services","../services")
-dao = imp.load_package("dao","../dao")
+#dao = imp.load_package("dao","../dao")
 
 #from dao.UserDAO import registerUser
-from beans.User import User
-from services.IndexService import downloadLink,parseHTML, tokenize
-from dao.UserDAO import registerUser, displayUsers
+#from beans.User import User
+#from services.IndexService import downloadLink,parseHTML, tokenize
+#from dao.UserDAO import registerUser, displayUsers
 routes = Blueprint("routes",__name__)
 
 
 @routes.route("/")
 def default():
-    user = User("arthur","arthur.grevin@gmail.com","mdp")
-    user.history.append("history1")
-    user.history.append("history2")
-    return jsonify(user.serialize())
+    return "test"
 
 @routes.route("/absorber")
 def linkAbsorber():
-    return parseHTML("https://fr.wikipedia.org/wiki/Pharaon")
+    return "test2"
 
 @routes.route("/register/<string:json>",methods=['POST','PUT'])
 def register():
-    user = request.get_json(force=True)
-    user = User(user['login'],user['email'],user['password'])
-    registerUser(user)
+    #user = request.get_json(force=True)
+    #user = User(user['login'],user['email'],user['password'])
+    #registerUser(user)
     return "test"
 
 @routes.route("/test2")
 def test2():
-    displayUsers()
+    #displayUsers()
     return "test2"
 
 @routes.route("/tokenize")
 def tokenizeText():
-    return tokenize("http://www.theverge.com/2015/12/10/9880594/microsoft-xbox-games-2016-interview-crackdown-3-quantum-break-recore")
+    #return tokenize("http://www.theverge.com/2015/12/10/9880594/microsoft-xbox-games-2016-interview-crackdown-3-quantum-break-recore")
+    return "aa"
